ffc6a62e5ec3ab2f3143751e05323b232a5fa145
rei <h.watanabe360@gmail.com>
2025-06-17 23:37:31 +0900

    style: npm i; npm run format; npm run lint; npm run type-check

--- frontend/src/components/TheWelcome.vue
+++ frontend/src/components/TheWelcome.vue
@@ -31,9 +31,10 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
     <a href="https://vite.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
     recommended IDE setup is
     <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a>
-    +
-    <a href="https://github.com/vuejs/language-tools" target="_blank" rel="noopener">Vue - Official</a>. If
-    you need to test your components and web pages, check out
+    + npm i; npm run format; npm run lint; npm run type-check
+    <a href="https://github.com/vuejs/language-tools" target="_blank" rel="noopener"
+      >Vue - Official</a
+    >. If you need to test your components and web pages, check out
     <a href="https://vitest.dev/" target="_blank" rel="noopener">Vitest</a>
     and
     <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a>
