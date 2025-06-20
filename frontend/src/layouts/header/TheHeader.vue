b7debd80a12c230f735bd3b1f55a849ac2365230
rei <h.watanabe360@gmail.com>
2025-06-21 03:29:43 +0900

    feat: 空のレイアウトコンポーネントと各ページのコンポーネントを作成

--- frontend/src/layouts/header/TheHeader.vue
+++ frontend/src/layouts/header/TheHeader.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
-import HelloWorld from './HelloWorld.vue'
 </script>
 
 <template>
@@ -8,11 +7,10 @@ import HelloWorld from './HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      header
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
