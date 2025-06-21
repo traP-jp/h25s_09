0bcd324cda249b44465a29a64ba4af3088f17d39
YuHima03 <yuhima03@gmail.com>
2025-06-21 17:22:47 +0900

    🏗️ 大改造

--- backend/domain/reaction.go
+++ backend/domain/reaction.go
@@ -8,6 +8,12 @@ import (
 
 type MessageReaction struct {
 	MessageID uuid.UUID
-	UserName   uuid.UUID
+	UserName  uuid.UUID
+	CreatedAt time.Time
+}
+
+type UserReactionToMessage struct {
+	MessageID uuid.UUID
+	Username  uuid.UUID
 	CreatedAt time.Time
 }
