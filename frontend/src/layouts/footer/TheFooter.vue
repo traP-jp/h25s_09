fc4177b0c90c7c04e5db2841479d9748c3c81af1
{you6174} <honma.y.fda3@m.isct.ac.jp>
2025-06-21 17:15:30 +0900

    footerの開発（BottomBarnの作成）

--- frontend/src/layouts/footer/TheFooter.vue
+++ frontend/src/layouts/footer/TheFooter.vue
@@ -1,51 +1,82 @@
-<script lang="ts" setup>
-defineProps<{
-  "/": string 
-  "/achievement": string
-  "/profile": string
-  "iconHome" : string
-  "iconAchievement": string
-  "iconUser": string
-  "ホーム": string  
-  "アチーブメント": string
-  "プロフィール": string
-}>()
+<script setup lang="ts">
+// ナビゲーションアイテムのデータは、スクリプト内に配列として持ちます。
+const navItems = [
+  { to: '/', iconName: 'icon-home', label: 'ホーム' },
+  { to: '/achievement', iconName: 'icon-achievement', label: 'アチーブメント' },
+  { to: '/profile', iconName: 'icon-user', label: 'プロフィール' },
+];
 </script>
 
 <template>
-    <router-link :to="to" class="tab-item" active-class="is-active" >
-        <i :class="['',iconHome]"></i>
-        <span class="">{{"ホーム"}}</span>
+  <footer :class="$style.bottomBar">
+    <router-link
+      v-for="item in navItems"
+      :key="item.to"
+      :to="item.to"
+      :class="$style.tabItem"
+      active-class="is-active"
+    >
+      <i :class="[$style.icon, item.iconName]"></i>
+      <span :class="$style.label">{{ item.label }}</span>
     </router-link>
-    <router-link :to="achievement" class="tab-item" active-class="is-active" >
-        <i :class="['',iconAchievement]"></i>
-        <span class="">{{"アチーブメント"}}</span>
-    </router-link>
-    <router-link :to="profile" class="tab-item" active-class="is-active" >
-        <i :class="['',iconUser]"></i>
-        <span class="">{{"プロフィール"}}</span>
-    </router-link>
+  </footer>
 </template>
 
 <style lang="scss" module>
+/*
+ * 全てのスタイルをこのコンポーネント内にまとめて記述します。
+ */
 
-.tab-item{
-   display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;   
-    flex-grow: 1; 
-    color: #ff0000; 
-    text-decoration: none;
+/* 親コンテナのスタイル */
+.bottomBar {
+  /* --- 位置とサイズ --- */
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  box-sizing: border-box;
+
+  /* --- 見た目と中身のレイアウト --- */
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #048004;
+  border-top: 1px solid #095709;
+  z-index: 1000;
+
+  /* --- 高さ と セーフエリア対応 --- */
+  height: 60px;
+  padding-bottom: env(safe-area-inset-bottom);
+}
+
+.tabItem {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 1;
+  height: 100%;
+  color: #f6f6f6;
+  text-decoration: none;
+  transition: color 0.2s;
+
+  &.is-active {
+     color: #0d39d9; /* アクティブ時の色 */
+    font-weight: 600;
+  }
 }
 .icon {
-  font-size: 240px;
+  font-size: 24px;
 }
+
 .label {
-  font-size: 10px;
+  font-size: 20px;
+  margin-top: 2px;
 }
-.is-active{
-    color: #ff0000;
-    font-weight: 600;
-}
+/*
+ * 
+active-classで指定したクラス名はグローバルなクラスとして扱われるため、
+ * CSSモジュール内からスタイルを当てるには :global() で囲みます。
+ */
+
 </style>
