0cee2618d4b1596dc501240a6d54a7b7512d87b8
ynta-3 <dashou47@gmail.com>
2025-06-21 13:52:37 +0900

    メッセージ時刻を生成するように DBからうまく持ってこれなかったのを修正 uuid生成方法変更

--- backend/repository/messages.go
+++ backend/repository/messages.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"time"
 	"github.com/google/uuid"
 
 	"github.com/traP-jp/h25s_09/domain"
@@ -11,13 +12,24 @@ type MessageRepository interface {
 	GetMessageByID(id uuid.UUID) (*domain.Message, error)
 }
 
+type Message struct {
+    ID        uuid.UUID `db:"id"`
+    Author    string    `db:"author"`
+    Content   string    `db:"content"`
+    ParentID  uuid.UUID `db:"parent_id"`
+    CreatedAt time.Time `db:"created_at"`
+    UpdatedAt time.Time `db:"updated_at"`
+}
+
 func (r *repositoryImpl) CreateMessage(author, content string, parentID uuid.UUID) (*domain.Message, error) {
 	message := &domain.Message{
-        ID:       uuid.New(),
-        Author:   author,
-        Content:  content,
-        ParentID: parentID,
-    }
+		ID:        uuid.Must(uuid.NewV7()),
+		Author:    author,
+		Content:   content,
+		ParentID:  parentID,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
 
 	// データベースに保存
 	_, err := r.db.Exec("INSERT INTO messages (id, author, message, replies_id) VALUES (?, ?, ?, ?)",
@@ -31,18 +43,28 @@ func (r *repositoryImpl) CreateMessage(author, content string, parentID uuid.UUI
 }
 
 func (r *repositoryImpl) GetMessageByID(id uuid.UUID) (*domain.Message, error) {
-	message := &domain.Message{
+	message := &Message{
 		ID:       id,
 		Author:   "",
 		Content:  "",
 		ParentID: uuid.Nil,
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
 	}
 
 	// データベースからメッセージを取得しmessageに格納
-	err := r.db.Get(message, "SELECT id, author, message, replies_id FROM messages WHERE id = ?", id)
+	err := r.db.Get(message, "SELECT id, author, message, replies_id, created_at, updated_at FROM messages WHERE id = ?", id)
 	if err != nil {
 		return nil, err // エラーが発生した場合はnilを返す
 	}
 
-	return message, nil
+	// domain.Messageに変換して返す
+	return &domain.Message{
+		ID:       message.ID,
+		Author:   message.Author,
+		Content:  message.Content,
+		ParentID: message.ParentID,
+		CreatedAt: message.CreatedAt,
+		UpdatedAt: message.UpdatedAt,
+	}, nil
 }
