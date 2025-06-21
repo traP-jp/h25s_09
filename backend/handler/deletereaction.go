af966cf8e4016b26717fc23a5f3ee77cb8a59bc0
ebisen1444 <0294ryotaro@gmail.com>
2025-06-21 22:50:01 +0900

    確認お願いします

---
+++ backend/handler/deletereaction.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/traP-jp/h25s_09/domain"
+)
+
+func (h *handler) DeleteMessagesHandler(c echo.Context) error {
+	//idを取得してuuid型に
+	ID, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid ID")
+	}
+	// idのメッセージがそもそも存在するか
+	_, err = h.repo.GetMessageByID(ID)
+	if err != nil {
+		if errors.Is(err, domain.ErrNotFound) {
+			return echo.NewHTTPError(http.StatusNotFound, "id not found")
+		} //404用
+		c.Logger().Error("Failed to retrieve id:", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to retrieve id")
+	} //404以外は500に
+	//ユーザーネームの取得
+	usernameRaw := c.Get("username")
+	username, ok := usernameRaw.(string)
+	if !ok || username == "" {
+		return echo.NewHTTPError(http.StatusUnauthorized, "unauthorized")
+	}
+	//リアクションを削除
+	err = h.repo.DeleteMessageReaction(ID, username)
+	if err != nil {
+		c.Logger().Error("Failed to retrieve id:", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to retrieve id")
+	}
+	//リアクションの数の取得
+	s, err := h.repo.GetReactionsToMessage(ID)
+	if err != nil {
+		c.Logger().Error("Failed to get reactions:", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get reactions")
+	}
+	count := len(s)
+	//自身のリアクションの有無
+	myreaction := containsb(s, username)
+	res := map[string]interface{}{
+		"count":      count,
+		"myReaction": myreaction,
+	}
+	return c.JSON(http.StatusOK, res)
+}
+func containsb(slice []*domain.MessageReaction, target string) bool {
+	for _, v := range slice {
+		if v.Username == target {
+			return true
+		}
+	}
+	return false
+}
