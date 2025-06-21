70affb30df10e7f9ecb7d6b017da96b8d3904997
rei <h.watanabe360@gmail.com>
2025-06-21 09:27:45 +0900

    feat: レスポンシブデザインを実装、各コンポーネントを追記

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import TheFooter from '@/layouts/footer/TheFooter.vue'
 import TheHeader from '@/layouts/header/TheHeader.vue'
+import TheSidebar from '@/layouts/sidebar/TheSidebar.vue'
+import { useBreakpoints } from '@vueuse/core'
 import { RouterView } from 'vue-router'
+
+const breakpoints = useBreakpoints({
+  mobile: 0,
+  tablet: 768,
+})
+
+const isDesktop = breakpoints.greaterOrEqual('tablet')
 </script>
 
 <template>
   <TheHeader />
+  <TheSidebar v-if="isDesktop" />
+  <TheFooter v-else />
   <RouterView />
 </template>
---
+++ frontend/src/layouts/sidebar/TheSidebar.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>sidebar</template>
+
+<style lang="scss" module></style>
