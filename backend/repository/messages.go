b95289e297323860bd338f4f3de9aaad3448a1e7
YuHima03 <yuhima03@gmail.com>
2025-06-21 12:02:09 +0900

    ✨ 仮実装

--- backend/repository/messages.go
+++ backend/repository/messages.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
 
 	"github.com/traP-jp/h25s_09/domain"
 )
@@ -9,4 +10,20 @@ import (
 type MessageRepository interface {
 	CreateMessage(author, content string, parentID uuid.UUID) (*domain.Message, error)
 	GetMessageByID(id uuid.UUID) (*domain.Message, error)
+}
+
+type messageRepositoryImpl struct {
+	db *sqlx.DB
+}
+
+func NewMessageRepository(db *sqlx.DB) MessageRepository {
+	return &messageRepositoryImpl{db: db}
+}
+
+func (r *messageRepositoryImpl) CreateMessage(author, content string, parentID uuid.UUID) (*domain.Message, error) {
+	return nil, domain.ErrNotImplemented
+}
+
+func (r *messageRepositoryImpl) GetMessageByID(id uuid.UUID) (*domain.Message, error) {
+	return nil, domain.ErrNotImplemented
 }
--- backend/repository/reaction.go
+++ backend/repository/reaction.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
 	"github.com/traP-jp/h25s_09/domain"
 )
 
@@ -9,4 +10,24 @@ type MessageRactionRepository interface {
 	GetMessageReaction(messageID uuid.UUID) (*domain.MessageReaction, error)
 	InsertMessageReaction(messageID uuid.UUID) (*domain.MessageReaction, error)
 	DeleteMessageReaction(messageID uuid.UUID) (*domain.MessageReaction, error)
+}
+
+type messageReactionRepositoryImpl struct {
+	db *sqlx.DB
+}
+
+func NewMessageReactionRepository(db *sqlx.DB) MessageRactionRepository {
+	return &messageReactionRepositoryImpl{db: db}
+}
+
+func (r *messageReactionRepositoryImpl) GetMessageReaction(messageID uuid.UUID) (*domain.MessageReaction, error) {
+	return nil, domain.ErrNotImplemented
+}
+
+func (r *messageReactionRepositoryImpl) InsertMessageReaction(messageID uuid.UUID) (*domain.MessageReaction, error) {
+	return nil, domain.ErrNotImplemented
+}
+
+func (r *messageReactionRepositoryImpl) DeleteMessageReaction(messageID uuid.UUID) (*domain.MessageReaction, error) {
+	return nil, domain.ErrNotImplemented
 }
