cd6fdaf9a1099bdcb05b7fcca2d8c13fe3507c89
ebisen1444 <0294ryotaro@gmail.com>
2025-06-21 18:54:58 +0900

    a

--- backend/handler/reaction.go
+++ backend/handler/reaction.go
@@ -9,12 +9,10 @@ import (
 )
 
 func (h *handler) ReactionsGetter(c echo.Context) error {
-
 	ID, err := uuid.Parse(c.Param("id"))
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "invalid ID")
 	}
-
 	//IDが存在しているか
 	_, err = h.repo.GetMessageByID(ID)
 	if err != nil {
@@ -24,23 +22,18 @@ func (h *handler) ReactionsGetter(c echo.Context) error {
 	var count int
 	s, _ := h.repo.GetReactionsToMessage(ID)
 	count = len(s)
-
 	//ユーザーネームの取得
 	usernameRaw := c.Get("username")
 	username, ok := usernameRaw.(string)
 	if !ok || username == "" {
 		return echo.NewHTTPError(http.StatusUnauthorized, "unauthorized")
 	}
-
 	myreaction := contains(s, username)
-
 	res := map[string]interface{}{
 		"count":      count,
 		"myReaction": myreaction,
 	}
-
 	return c.JSON(http.StatusCreated, res)
-
 }
 func contains(slice []domain.MessageReaction, target string) bool {
 	for _, v := range slice {
