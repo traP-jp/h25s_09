762f2f1acf1f201f9d043dbbf8e794d337825b1f
iwnevaf <ryhaxa@gmail.com>
2025-06-21 11:52:47 +0900

    サイドバーを更新

--- frontend/src/layouts/sidebar/TheSidebar.vue
+++ frontend/src/layouts/sidebar/TheSidebar.vue
@@ -1,5 +1,42 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const tools = [
+  { name: 'timeline', path: '/timeline' },
+  { name: 'messageId', path: '/messageId' },
+  { name: 'messages', path: '/messages' },
+  { name: 'achievements', path: '/achievements' }
+];
 
-<template>sidebar</template>
 
-<style lang="scss" module></style>
+</script>
+
+<template>
+  <header>
+    <nav :class="$style.sidebar">
+      <div v-for="tool in tools" :key="tool.name" :class="$style.sidebar__item">
+        <router-link :class="$style.sidebar__link" :to="tool.path">{{ tool.name }}</router-link>
+      </div>
+
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" module>
+.sidebar {
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+}
+.sidebar__item {
+  padding: 10px;
+  text-decoration: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #f0f0f0;
+  }
+}
+.sidebar__link {
+  text-decoration: none;
+  color: inherit;
+  font-weight: bold;
+}
+</style>
