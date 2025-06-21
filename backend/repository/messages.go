4440660ae996be7f347ad903b5d0aa0a2d6cc219
ynta-3 <dashou47@gmail.com>
2025-06-21 12:51:43 +0900

    メッセージ作成、取得の操作を書いた

--- backend/repository/messages.go
+++ backend/repository/messages.go
@@ -12,9 +12,40 @@ type MessageRepository interface {
 }
 
 func (r *repositoryImpl) CreateMessage(author, content string, parentID uuid.UUID) (*domain.Message, error) {
-	return nil, domain.ErrNotImplemented
+	
+	message := &domain.Message{
+        ID:       uuid.New(),
+        Author:   author,
+        Content:  content,
+        ParentID: parentID,
+    }
+
+	// データベースに保存
+	_, err := r.db.Exec("INSERT INTO messages (id, author, message, replies_id) VALUES (?, ?, ?, ?)",
+		message.ID, message.Author, message.Content, message.ParentID,
+	)
+	
+	if err != nil {
+		return nil, err
+	}
+	return message, nil
 }
 
 func (r *repositoryImpl) GetMessageByID(id uuid.UUID) (*domain.Message, error) {
-	return nil, domain.ErrNotImplemented
+
+
+	message := &domain.Message{
+		ID:       id,
+		Author:   "",
+		Content:  "",
+		ParentID: uuid.Nil,
+	}
+
+	// データベースからメッセージを取得しmessageに格納
+	err := r.db.Get(message, "SELECT id, author, message, replies_id FROM messages WHERE id = ?", id)
+	if err != nil {
+		return nil, err // エラーが発生した場合はnilを返す
+	}
+
+	return message, nil
 }
