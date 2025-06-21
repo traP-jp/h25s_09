96e2dc6f595b60c3100b78590bb3434fae05d264
iwnevaf <ryhaxa@gmail.com>
2025-06-21 18:53:43 +0900

    SidebarItemコンポーネントのアイコン表示を動的に変更し、サイドバーのスタイルを調整

--- frontend/src/layouts/sidebar/SidebarItem.vue
+++ frontend/src/layouts/sidebar/SidebarItem.vue
@@ -1,17 +1,31 @@
 <template>
-  <RouterLink :class="$style.sidebar__link" :to="`${props.path}`">
-    <Icon :icon="`${props.icon}`" height="40px" width="40px" />
+  <RouterLink
+    :class="[$style.sidebar__link, { [$style.current]: isCurrent }]"
+    :to="`${props.path}`"
+  >
+    <Icon :icon="`${iconPath}`" height="40px" width="40px" />
     <span>{{ props.name }}</span>
   </RouterLink>
 </template>
 
 <script lang="ts" setup>
 import { Icon } from '@iconify/vue'
-
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 const props = defineProps({
   name: String,
   path: String,
   icon: String,
+})
+const route = useRoute()
+const currentPath = computed(() => route.path)
+const isCurrent = computed(() => {
+  return currentPath.value === props.path
+})
+const iconPath = computed(() => {
+  return isCurrent.value
+    ? `material-symbols:${props.icon}`
+    : `material-symbols-light:${props.icon}-outline`
 })
 </script>
 
@@ -25,11 +39,15 @@ const props = defineProps({
   gap: 10px;
   padding: 20px;
   color: inherit;
-  font-weight: bold;
+  font-weight: normal;
   font-size: 150%;
   text-decoration: none;
   &:hover {
     background-color: var(--color-shadow-light);
   }
+}
+
+.current {
+  font-weight: bold;
 }
 </style>
