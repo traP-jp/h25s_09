50074efa19a65325d72d773cef60ebfc2430859a
YuHima03 <yuhima03@gmail.com>
2025-06-21 15:15:02 +0900

    🔊 エラーログ

--- backend/handler/image.go
+++ backend/handler/image.go
@@ -18,6 +18,7 @@ func (h *handler) GetMessageImageHandler(ctx echo.Context) error {
 		if err == domain.ErrNotFound {
 			return echo.NewHTTPError(http.StatusNotFound, "image not found")
 		}
+		ctx.Logger().Error("Failed to retrieve image:", err)
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to retrieve image")
 	}
 	return ctx.Blob(http.StatusOK, image.Mime, image.Data)
