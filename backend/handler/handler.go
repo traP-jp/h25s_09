54a83b5f8e76f89d00d2a24041020ea2463d2ff5
ebisen1444 <0294ryotaro@gmail.com>
2025-06-21 18:47:52 +0900

    あ

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -30,6 +30,7 @@ func Start() {
 		g.GET("/images/:id", h.GetMessageImageHandler)
 		g.GET("/me", h.GetMeHandler)
 		g.GET("/try-achieve/:id", h.TryAchieveHandler)
+		g.GET("/messages/:id/reaction",h.ReactionsGetter)
 	}
 
 	e.Logger.Fatal(e.Start(":8080"))
---
+++ backend/handler/reaction.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"net/http"
+	// "strconv"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/traP-jp/h25s_09/domain"
+)
+
+func (h *handler) ReactionsGetter(c echo.Context) error {
+
+	ID, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid ID")
+	}
+
+	//IDが存在しているか
+	_, err = h.repo.GetMessageByID(ID)
+	if err != nil {
+		return c.JSON(http.StatusNotFound, map[string]string{"error": "ID is not founded"})
+	}
+//リアクションの数の取得
+	var count int
+	s,_:= h.repo.GetReactionsToMessage(ID)
+	count = len(s)
+
+	//ユーザーネームの取得
+	usernameRaw := c.Get("username")
+	username, ok := usernameRaw.(string)
+	if !ok || username == "" {
+		return echo.NewHTTPError(http.StatusUnauthorized, "unauthorized")
+	}
+
+ 
+	myreaction := contains(s,username)
+
+	res := map[string]interface{}{
+		"count":      count,
+		"myReaction": myreaction,
+	}
+
+	return c.JSON(http.StatusCreated, res)
+
+}
+func contains(slice []domain.MessageReaction, target string) bool {
+	for _, v := range slice {
+		if v.Username == target {
+			return true
+		}
+	}
+	return false
+}
