00ba9d6908d367d3056d6b3fa8602434e54450c6
rei <h.watanabe360@gmail.com>
2025-06-21 11:33:36 +0900

    npm run format

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -3,7 +3,7 @@ import { defineStore, storeToRefs } from 'pinia'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function defineComposable<Id extends string, SS extends () => any>(
   id: Id,
-  storeSetup: SS
+  storeSetup: SS,
 ): () => ReturnType<SS> {
   const piniaStore = defineStore(id, storeSetup)
 
