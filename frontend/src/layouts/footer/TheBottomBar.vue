3d2bdf79760e0629b830f1e3f139d237e12b9ea3
{you6174} <honma.y.fda3@m.isct.ac.jp>
2025-06-21 12:34:10 +0900

    footerの開発

--- frontend/src/layouts/footer/TheBottomBar.vue
+++ frontend/src/layouts/footer/TheBottomBar.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup></script>
 
 <template>
-    <footer class="bottom-bar">
-    <TabBarItem to="/" iconName="iconHome" label="ホーム" />
-    <TabBarItem to="/achievement" iconName="iconAchievement" label="アチーブメント"/>
-    <TabBarItem to="/profile" iconName="iconUser" label="プロフィール" />
+    <footer class="$style.bottom-bar">
+    <TabBarItem to="/" icon-name="icon-home" label="ホーム" />
+    <TabBarItem to="/achievement" icon-name="icon-achievement" label="アチーブメント"/>
+    <TabBarItem to="/profile" icon-name="icon-user" label="プロフィール" />
   </footer>
 </template>
 
@@ -16,6 +16,7 @@
    left: 0;
    width:100%;
    height: 60px;
+   padding-bottom: env(safe-area-inset-bottom);
    display: flex;
    justify-content: space-around; 
    align-items: center;
