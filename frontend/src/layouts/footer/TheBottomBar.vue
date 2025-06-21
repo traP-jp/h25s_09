9ca62618943615b9b2852b39f05a0616c8c25409
{you6174} <honma.y.fda3@m.isct.ac.jp>
2025-06-21 11:51:54 +0900

    フッターの設定

---
+++ frontend/src/layouts/footer/TheBottomBar.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup></script>
+
+<template>
+    <footer class="bottom-bar">
+    <TabBarItem to="/" iconName="iconHome" label="ホーム" />
+    <TabBarItem to="/achievement" iconName="iconAchievement" label="アチーブメント"/>
+    <TabBarItem to="/profile" iconName="iconUser" label="プロフィール" />
+  </footer>
+</template>
+
+<style lang="scss" module>
+.bottom-bar{
+   position: fixed;
+   top: auto;
+   bottom: 0;
+   left: 0;
+   width:100%;
+   height: 60px;
+   display: flex;
+   justify-content: space-around; 
+   align-items: center;
+   z-index: 1000;
+   background-color: #1f3d1f ;
+   border-top: 1px solid #12ae12;
+}
+</style>
--- frontend/src/layouts/footer/TheFooter.vue
+++ frontend/src/layouts/footer/TheFooter.vue
@@ -1,5 +1,51 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+defineProps<{
+  "/": string 
+  "/achievement": string
+  "/profile": string
+  "iconHome" : string
+  "iconAchievement": string
+  "iconUser": string
+  "ホーム": string  
+  "アチーブメント": string
+  "プロフィール": string
+}>()
+</script>
 
-<template>footer</template>
+<template>
+    <router-link :to="to" class="tab-item" active-class="is-active" >
+        <i :class="['',iconHome]"></i>
+        <span class="">{{"ホーム"}}</span>
+    </router-link>
+    <router-link :to="achievement" class="tab-item" active-class="is-active" >
+        <i :class="['',iconAchievement]"></i>
+        <span class="">{{"アチーブメント"}}</span>
+    </router-link>
+    <router-link :to="profile" class="tab-item" active-class="is-active" >
+        <i :class="['',iconUser]"></i>
+        <span class="">{{"プロフィール"}}</span>
+    </router-link>
+</template>
 
-<style lang="scss" module></style>
+<style lang="scss" module>
+
+.tab-item{
+   display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;   
+    flex-grow: 1; 
+    color: #ff0000; 
+    text-decoration: none;
+}
+.icon {
+  font-size: 240px;
+}
+.label {
+  font-size: 10px;
+}
+.is-active{
+    color: #ff0000;
+    font-weight: 600;
+}
+</style>
