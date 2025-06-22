1c97c588214ff6bcd85d505c8dadaffe5f0c03bf
ynta-3 <dashou47@gmail.com>
2025-06-22 13:53:41 +0900

    たまに1970/01/01になる

--- backend/handler/messages.go
+++ backend/handler/messages.go
@@ -315,7 +315,25 @@ func (h *handler) GetMessageHandler(c echo.Context) error {
 		},
 		Replies:   repliesList,
 		CreatedAt: msg.CreatedAt,
-	})
+		})
+	}
+
+	bug, shouldDispatch = u.DetermineDispatchBugAndRecord(1, h.repo)
+	if shouldDispatch {
+		c.Logger().Info("Bug dispatched:", bug.Name)
+		msg.CreatedAt = time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+		return c.JSON(http.StatusOK, &messageDetail{
+		ID:      ID,
+		Author:  msg.Author,
+		Content: msg.Content,
+		ImageID: imageID,
+		Reactions: reactions{
+			Count:      reactionsCount,
+			MyReaction: myReaction,
+		},
+		Replies:   repliesList,
+		CreatedAt: msg.CreatedAt,
+		})
 	}
 
 	return c.JSON(http.StatusOK, &messageDetail{
