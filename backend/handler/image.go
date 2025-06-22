a1760751d466dc5fc718612e058900194a9f2e8f
YuHima03 <yuhima03@gmail.com>
2025-06-22 13:39:39 +0900

    🐛 ごめｎ

--- backend/handler/image.go
+++ backend/handler/image.go
@@ -19,7 +19,7 @@ func (h *handler) GetMessageImageHandler(ctx echo.Context) error {
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid image ID")
 	}
-	imageobj, err := h.repo.GetMessageImage(imageID)
+	imageObj, err := h.repo.GetMessageImage(imageID)
 	if err != nil {
 		if errors.Is(err, domain.ErrNotFound) {
 			return echo.NewHTTPError(http.StatusNotFound, "image not found")
@@ -29,29 +29,29 @@ func (h *handler) GetMessageImageHandler(ctx echo.Context) error {
 	}
 
 	if utils.DetermineDispatchBug(ctx, h.repo, h.ss, 3) {
-		if len(image.Data) > 0 {
-			image.Data = append(image.Data[:len(image.Data)/2], make([]byte, len(image.Data)/2)...)
+		if len(imageObj.Data) > 0 {
+			imageObj.Data = append(imageObj.Data[:len(imageObj.Data)/2], make([]byte, len(imageObj.Data)/2)...)
 		}
-	}else{
-	if _, ok := utils.DetermineDispatchBugAndRecord(8, h.repo); ok {
-		// []byte → image.Image に変換
-		imgDecoded, _, err := image.Decode(bytes.NewReader(imageobj.Data))
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, "failed to decode image")
+	} else {
+		if _, ok := utils.DetermineDispatchBugAndRecord(8, h.repo); ok {
+			// []byte → image.Image に変換
+			imgDecoded, _, err := image.Decode(bytes.NewReader(imageObj.Data))
+			if err != nil {
+				return echo.NewHTTPError(http.StatusInternalServerError, "failed to decode image")
+			}
+			//resize
+			m := resize.Resize(120, 0, imgDecoded, resize.NearestNeighbor)
+			//image.Image→[]byte
+			var buf bytes.Buffer
+			err = jpeg.Encode(&buf, m, nil)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusInternalServerError, "failed to encode resized image")
+			}
+			// Dataを書き換え
+			imageObj.Data = buf.Bytes()
+			imageObj.Mime = "image/jpeg" // MIMEも変更
 		}
-		//resize
-		m := resize.Resize(120, 0, imgDecoded, resize.NearestNeighbor)
-		//image.Image→[]byte
-		var buf bytes.Buffer
-		err = jpeg.Encode(&buf, m, nil)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, "failed to encode resized image")
-		}
-		// Dataを書き換え
-		imageobj.Data = buf.Bytes()
-		imageobj.Mime = "image/jpeg" // MIMEも変更
 	}
-}
 
-	return ctx.Blob(http.StatusOK, imageobj.Mime, imageobj.Data)
+	return ctx.Blob(http.StatusOK, imageObj.Mime, imageObj.Data)
 }
