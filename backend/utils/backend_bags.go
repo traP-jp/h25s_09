6383964338a8388a8d599c04d4f275a9ddde5e83
ynta-3 <dashou47@gmail.com>
2025-06-22 13:50:37 +0900

    たまに１年後になる

--- backend/utils/backend_bags.go
+++ backend/utils/backend_bags.go
@@ -17,18 +17,18 @@ type Bug struct {
 
 var (
 	BackendBugs = map[int]Bug{
-		1:   {Name: "投稿の日時がおかしい"},
+		1:   {Name: "投稿の日時がおかしい", Probability: 0.1},
 		2:   {Name: "データが取得できない"},
 		3:   {Name: "画像が半分しか表示されない", Probability: 0.1, ValidTimeSec: 60},
 		4:   {Name: "同じ投稿が複数ある"},
 		5:   {Name: "API制限"},
 		6:   {Name: "ユーザーが全部同じに見える"},
-		7:   {Name: "リプがさらに増殖する"},
+		7:   {Name: "リプがさらに増殖する", Probability: 0.8},
 		8:   {Name: "画質が悪い"},
 		9:   {Name: "阿部寛で爆速に"},
 		10:  {Name: "レスポンスが遅い"},
 		11:  {Name: "いいねが無限に増やせる"},
-		100: {Name: "リプ増殖が始まる"},
+		100: {Name: "リプ増殖が始まる", Probability: 0.65},
 	}
 )
 
