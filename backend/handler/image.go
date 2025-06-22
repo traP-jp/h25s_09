7cad2bda71979f52b6fb6bfd9b61f0d121a58bc6
YuHima03 <yuhima03@gmail.com>
2025-06-22 11:14:53 +0900

    ✨ 画像が半分になるバグ

--- backend/handler/image.go
+++ backend/handler/image.go
@@ -7,6 +7,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/traP-jp/h25s_09/domain"
+	"github.com/traP-jp/h25s_09/utils"
 )
 
 func (h *handler) GetMessageImageHandler(ctx echo.Context) error {
@@ -22,5 +23,12 @@ func (h *handler) GetMessageImageHandler(ctx echo.Context) error {
 		ctx.Logger().Error("Failed to retrieve image:", err)
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to retrieve image")
 	}
+
+	if _, ok := utils.DetermineDispatchBugAndRecord(3, h.repo); ok {
+		if len(image.Data) > 0 {
+			image.Data = append(image.Data[:len(image.Data)/2], make([]byte, len(image.Data)/2)...)
+		}
+	}
+
 	return ctx.Blob(http.StatusOK, image.Mime, image.Data)
 }
