07b826234469924b22e880bf013a18e3508f3fd5
{you6174} <honma.y.fda3@m.isct.ac.jp>
2025-06-22 09:18:15 +0900

    footer: Add footer component

--- frontend/src/layouts/footer/TheFooter.vue
+++ frontend/src/layouts/footer/TheFooter.vue
@@ -2,18 +2,23 @@
 import { Icon } from '@iconify/vue'
 import { userService } from '@/lib/apis/services.ts'
 import { onMounted, ref } from 'vue'
-import { computed } from 'vue'  
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
 // ユーザーIDの取得
 const userId = ref<string | null>(null)
 onMounted(async () => {
   userId.value = (await userService.getUserInfo()).traqId
-}) 
-const navItems = computed(()=>[
+})
+const navItems = computed(() => [
   { to: '/timeline', icon: 'mdi:home', label: 'ホーム' },
-  { to: '/achievements', icon: 'mdi:trophy', label: 'アチーブメント' },
-  { to: `/user/${userId.value}`, icon: 'mdi:account', label: 'プロフィール' },
+  { to: `/users/${userId.value}/achievements`, icon: 'mdi:trophy', label: 'アチーブメント' },
+  { to: `/users/${userId.value}/messages`, icon: 'mdi:account', label: 'プロフィール' },
 ])
+const route = useRoute()
+const isCurrent = computed(() => {
+  return route.path === '/timeline' || route.path.startsWith('/users/')
+})
 </script>
 
 <template>
@@ -22,7 +27,7 @@ const navItems = computed(()=>[
       v-for="item in navItems"
       :key="item.to"
       :to="item.to"
-      :class="$style.tabItem"
+      :class="[$style.tabItem, { [$style.current]: isCurrent }]"
       active-class="is-active"
     >
       <Icon :icon="item.icon" :class="$style.icon" />
@@ -52,7 +57,7 @@ const navItems = computed(()=>[
   bottom: 0;
   width: 100%;
   box-shadow: var(--shadow);
-    z-index: 1000;
+  z-index: 1000;
 }
 .tabItem {
   display: flex;
@@ -62,31 +67,41 @@ const navItems = computed(()=>[
   color: var(--colorPrimary);
   font-size: 12px;
   padding: 5px;
-    transition: color 0.3s ease, transform 0.3s ease;
-    cursor: pointer;
-    border-radius: 10px;
-    &:hover {
+  transition:
+    color 0.3s ease,
+    transform 0.3s ease;
+  cursor: pointer;
+  border-radius: 10px;
+  &:hover {
     color: var(--colorActive);
-    transform: scale(2.0);
+    transform: scale(2);
     box-shadow: var(--shadow);
-    transition: color 0.3s ease, transform 0.3s ease, box-shadow 0.3s ease; 
-    }
-    &.isActive {
+    transition:
+      color 0.3s ease,
+      transform 0.3s ease,
+      box-shadow 0.3s ease;
+  }
+  &.isActive {
     color: var(--colorActive);
     transform: scale(1.1);
     font-weight: bold;
     box-shadow: var(--shadow);
-    transition: color 0.3s ease, transform 0.3s ease, box-shadow 0.3s ease;
-}
+    transition:
+      color 0.3s ease,
+      transform 0.3s ease,
+      box-shadow 0.3s ease;
+  }
 }
 .icon {
   font-size: 24px;
-  
 }
 
 .label {
   font-size: 10px;
   margin-top: 2px;
 }
-
+.current {
+  color: var(--colorPrimary);
+  font-weight: bold;
+}
 </style>
