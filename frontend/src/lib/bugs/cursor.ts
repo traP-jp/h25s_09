fce43322b9aa9cf4cfab721afa78dda244702fcc
iwnevaf <ryhaxa@gmail.com>
2025-06-22 11:29:22 +0900

    ✨ d3-randomライブラリを追加し、カーソルの読み込み状態を管理する機能を実装

---
+++ frontend/src/lib/bugs/cursor.ts
@@ -0,0 +1,17 @@
+import { ref } from 'vue'
+import { randomExponential } from 'd3-random'
+
+const isCursorLoading = ref(false)
+
+export const useCursorLoading = () => {
+  return { isCursorLoading: isCursorLoading }
+}
+
+const toggleCursorLoading = () => {
+  isCursorLoading.value = !isCursorLoading.value
+
+  const delay = isCursorLoading.value ? randomExponential(1000)() : randomExponential(5000)()
+  setTimeout(toggleCursorLoading, delay)
+}
+
+toggleCursorLoading()
